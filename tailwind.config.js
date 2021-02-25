aac70d405e54d31c030abff596b908260a95ba5d
Eden Kupermintz <edenk@liveperson.com)>
2021-02-25 16:31:55 +0200

    readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        apricot: '#FCDBB0',
+        eagle: '#114457',
+        gold: '#C79036',
+        brass: '#CA9082',
+        chestnut: '#9A3D35',
+        carmine: '#0e141f',
+        tiffany: '#00BABA',
+        carmine: '#0e141f',
+        tangerine: '#F4998D',
+        begonia: '#F97373',
+        quicksilver: '#AFA694',
+        sunset: '#FE5F55',
+      },
+      fontFamily: {
+        'main': ['Montserrat', 'Helvetica'],
+      },
+    },
   },
   variants: {},
   plugins: []
