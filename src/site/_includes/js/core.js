30c1e519429a2e2abda950eff0faccdfa50f9859
Eden Kupermintz <edenk@liveperson.com)>
2021-03-02 13:14:57 +0200

    image fade on scroll

--- src/site/_includes/js/core.js
+++ src/site/_includes/js/core.js
@@ -8,3 +8,20 @@ function btnHandler(selector, callback) {
   }, false);
 }
 
+window.onscroll=function(){changeMenu()}
+
+function changeMenu()
+{
+    var scrollBarPosition = window.pageYOffset | document.body.scrollTop;
+
+    // At specifiv position do what you want 
+    if(scrollBarPosition > 500) {
+        var body = document.getElementById('mainBody');
+        body.classList.add('backgroundSecond');
+    }
+    else {
+      var body = document.getElementById('mainBody');
+      body.classList.remove('backgroundSecond');
+    }
+}
+
